5e8e4bc6f967bb6950ad5927d6b7fc884b930eaf
BobbyLin <linzhangsheng23@gmail.com>
2025-05-27 23:00:52 +0800

    chore: add ui

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="h-screen bg-neutral-950 text-white">
+    <slot />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['~/assets/css/tailwind.css'],
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui',
+  },
 })
